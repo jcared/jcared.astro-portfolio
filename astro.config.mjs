d315b020fe49b7d1fa2b1514dd92f59cb7d86c7f
Jaden Curley <86536411+jcared@users.noreply.github.com>
2022-06-21 16:49:40 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
+import preact from '@astrojs/preact';
 export default {
   buildOptions: {
     site: "https://jadencurley.me",
     sitemap: true,
   },
-  renderers: ["@astrojs/renderer-preact"],
+  integrations:[
+    preact(),
+  ],
 };
