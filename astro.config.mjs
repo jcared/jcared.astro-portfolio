c24a4ed563b5981490756aa98f9f87de83625528
Jaden Curley <86536411+jcared@users.noreply.github.com>
2022-06-21 17:01:19 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 import preact from '@astrojs/preact';
 
+// https://astro.build/config
 export default defineConfig({
-  buildOptions: {
-    site: "https://jadencurley.me",
-    sitemap: true,
-  },
-  integrations:[
-    preact(),
-  ],
+	// Enable Preact to support Preact JSX components.
+  site: "https://jadencurley.me",
+	integrations: [preact(),sitemap()],
 });
